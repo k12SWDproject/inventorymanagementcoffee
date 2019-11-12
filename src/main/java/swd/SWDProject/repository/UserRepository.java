51f468242ad8003b4defecfe79af52d8175cc971
ThanhNV <iamtheb2st1998@gmail.com>
2019-11-12 20:10:19 +0700

    Create Entity User config Database

---
+++ src/main/java/swd/SWDProject/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package swd.SWDProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import swd.SWDProject.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    public String getRoleByPasswordAndUsername(String password, String userName);
+
+}
