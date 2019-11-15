e2696d696fe9c1ea5d6cec4192a0316eec2e8d7f
ThanhNV <thanhnvse63366@fpt.edu.vn>
2019-11-15 17:58:34 +0700

    Sercurity Config

--- src/main/java/swd/SWDProject/SwdProjectApplication.java
+++ src/main/java/swd/SWDProject/SwdProjectApplication.java
@@ -1,5 +1,6 @@
 package swd.SWDProject;
 
+import com.google.common.collect.Lists;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +8,41 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+
+import java.util.List;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableWebSecurity
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SwdProjectApplication {
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+				.build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope
+				= new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Lists.newArrayList(
+				new SecurityReference("JWT", authorizationScopes));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwdProjectApplication.class, args);
---
+++ src/main/java/swd/SWDProject/service/imp/UserServiceImp.java
@@ -0,0 +1,41 @@
+package swd.SWDProject.service.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import swd.SWDProject.entity.User;
+import swd.SWDProject.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImp implements swd.SWDProject.service.imp.UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public String getRoleByUserNameAndPassword(String username, String password) {
+        try{
+            String role = userRepository.getRoleByPasswordAndUsername(password,username);
+            return role;
+        }finally {
+
+        }
+    }
+
+    @Override
+    public User createUser(User user) {
+        try {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+            return user;
+        }finally {
+
+        }
+    }
+
+}
