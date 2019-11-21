bf8eef79dedc5a55c854b41dfe3de34275863e3d
ThanhNV <thanhnvse63366@fpt.edu.vn>
2019-11-21 11:31:12 +0700

    Fix api by type

--- src/main/java/swd/SWDProject/constant/StringRS.java
+++ src/main/java/swd/SWDProject/constant/StringRS.java
@@ -7,9 +7,9 @@ public class StringRS {
     public static final String BEGIN_CONTROLLER = "Begin controller :";
     public static final String END_CONTROLLER = "End controller :";
 
-    public static final int WATER_TYPE = 24;
-    public static final int ELECTRIC_TYPE = 23;
-    public static final int ORTHER_TYPE = 25;
+    public static final int WATER_TYPE = 23;
+    public static final int ELECTRIC_TYPE = 25;
+    public static final int ORTHER_TYPE = 24;
     public static final int INTERNET_TYPE = 26;
 
 }
