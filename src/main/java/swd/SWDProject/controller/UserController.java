b68f0189567187765831fd48a470e07c96891b28
ThanhNV <thanhnvse63366@fpt.edu.vn>
2019-12-01 12:35:21 +0700

    quick fix api users

--- src/main/java/swd/SWDProject/controller/UserController.java
+++ src/main/java/swd/SWDProject/controller/UserController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,7 @@ import swd.SWDProject.service.UserService;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
