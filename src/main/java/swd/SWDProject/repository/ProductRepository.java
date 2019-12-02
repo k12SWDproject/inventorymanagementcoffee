dbad996daed42a5307e1190f6435d2c61ef93a5f
ThanhNV <thanhnvse63366@fpt.edu.vn>
2019-12-02 21:25:49 +0700

    Quick fix search Like

--- src/main/java/swd/SWDProject/repository/ProductRepository.java
+++ src/main/java/swd/SWDProject/repository/ProductRepository.java
@@ -1,12 +1,12 @@
 package swd.SWDProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import swd.SWDProject.entity.Product;
-import swd.SWDProject.entity.Receipt;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
 
 
 }
--- src/main/java/swd/SWDProject/service/imp/ProductServiceImp.java
+++ src/main/java/swd/SWDProject/service/imp/ProductServiceImp.java
@@ -4,16 +4,20 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import swd.SWDProject.config.sercurity.JWTVerifier;
 import swd.SWDProject.constant.StringRS;
 import swd.SWDProject.entity.Product;
+import swd.SWDProject.entity.Product_;
+import swd.SWDProject.entity.SpecificationBuilder;
 import swd.SWDProject.entity.User;
 import swd.SWDProject.filter.ProductFilter;
 import swd.SWDProject.repository.ProductRepository;
 import swd.SWDProject.repository.UserRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -29,12 +33,18 @@ public class ProductServiceImp implements swd.SWDProject.service.ProductService 
         try{
             log.info(StringRS.BEGIN_SERVICE + "getProducts");
             List<Product> products = null;
+            List<Specification<Product>> specs = new ArrayList<>();
             if(filter != null) {
                 ProductFilter productFilter = new ObjectMapper().readValue(filter, ProductFilter.class);
 
+                if(productFilter.getName() != null) {
+                    specs.add((root, cq, cb) -> {
+                        return  cb.like(cb.upper(root.get(Product_.NAME)), "%" + productFilter.getName() + "%" ,'\\');
+                    });
+                }
             }
 
-            products = productRepository.findAll();
+            products = productRepository.findAll(SpecificationBuilder.build(specs));
 
             return products;
         }finally {
